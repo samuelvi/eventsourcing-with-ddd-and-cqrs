866e18930cbee54952ee801a29e3147295dac541
Olga Girona <olgagironac@gmail.com>
2026-03-20 17:38:35 +0100

    feat: add new Event to store when there is no available candidates

---
+++ src/App/Domain/Derivation/Event/QuoteCandidatesNotFound.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Domain\Derivation\Event;
+
+final readonly class QuoteCandidatesNotFound
+{
+    public function __construct(
+        public string $bookingId,
+        public string $derivationRunId,
+        public string $correlationId,
+        public \DateTimeImmutable $occurredOn = new \DateTimeImmutable(),
+    ) {}
+}
